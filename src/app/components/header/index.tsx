6bbd151d8ef30bfc8888459ad5425abe91cea472
israelcruzz <israelcruz.contato@gmail.com>
2024-06-10 20:27:32 -0300

    fix: link in image was updated

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -9,23 +9,37 @@ export const Header = () => {
 
   return (
     <header className="w-full flex justify-between items-center container">
-      <Image
-        src="/images/israelcruzz-logo.png"
-        alt="Logo to Israel Cruz"
-        width={56}
-        height={56}
-        quality={100}
-      />
+      <Link href="/">
+        <Image
+          src="/images/israelcruzz-logo.png"
+          alt="Logo to Israel Cruz"
+          width={56}
+          height={56}
+          quality={100}
+        />
+      </Link>
 
       <section className="flex gap-6">
         <Link href="/" className="flex gap-1">
           <span className="text-emerald-400">#</span>
-          <h1 className={`${path === '/' ? 'text-white' : 'text-gray-400'} font-medium text-base`}>Home</h1>
+          <h1
+            className={`${
+              path === "/" ? "text-white" : "text-gray-400"
+            } font-medium text-base`}
+          >
+            Home
+          </h1>
         </Link>
 
         <Link href="/projects" className="flex gap-1">
           <span className="text-emerald-400">#</span>
-          <h1 className={`${path === '/projects' ? 'text-white' : 'text-gray-400'} font-medium text-base`}>Projetos</h1>
+          <h1
+            className={`${
+              path === "/projects" ? "text-white" : "text-gray-400"
+            } font-medium text-base`}
+          >
+            Projetos
+          </h1>
         </Link>
       </section>
     </header>
